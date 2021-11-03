bbcecc9abee0ec47d68eafef93926b6180dfdbc3
valentincruz2005 <93301606+valentincruz2005@users.noreply.github.com>
2021-11-02 23:19:45 -0300

    Add files via upload

---
+++ traning 1/training.js
@@ -0,0 +1,35 @@
+//CONSIGNA1
+/*let number = (parseInt (prompt ("ESCRIBA EL RESULTADO DE 151X10")))
+if (number ==1510) {
+    alert("FELICIDADES, USTED RESOLVIO EL CALCULO") 
+}
+else {
+    alert ("ESE NO ES EL RESULTADO. SIGA INTENTADOLO")
+}*/
+
+
+
+
+
+//CONSIGNA 2
+let name1= (prompt ("ESCRIBA EL APELLIDO DEL ACTUAL PRESIDENTE DE LOS ESTADOS UNIDOS"))
+if ((name1=="Biden" ) || (name1=="BIDEN")) {
+    console.log ("USTED A ESCRITO EL NOMBRE DEL ACTUAL PREIDENTE DE LOS ESTADOS UNIDOS")
+}
+else {
+     console.log ("ESE NO ES, SIGA INTENTADOLO")
+}
+
+
+
+
+
+
+
+//CONSIGNA 3
+//let numero= (parseInt(prompt ("Escriba su edad")))
+//if ((numero>=0) && (numero <= 70)) {
+ //   alert ("USTED ES JOVEN")
+//}else {
+//    alert ("Usted esta en una edad avanzada")
+//}*/
